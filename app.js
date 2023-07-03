5c2524478a443bc11cbcdf03a0f269fe3777cacb
Joe Klein SEI <josephklein87@gmail.com>
2023-07-03 13:13:42 -0400

    completed chart

---
+++ app.js
@@ -0,0 +1,633 @@
+const data = [
+    {
+      "Date": "2023-04-20",
+      "Open": 130.149994,
+      "High": 130.979996,
+      "Low": 125.839996,
+      "Close": 126.360001,
+      "Adj Close": 124.660187,
+      "Volume": 9749600
+    },
+    {
+      "Date": "2023-04-21",
+      "Open": 126,
+      "High": 126.699997,
+      "Low": 125.269997,
+      "Close": 125.730003,
+      "Adj Close": 124.038658,
+      "Volume": 6725400
+    },
+    {
+      "Date": "2023-04-24",
+      "Open": 125.550003,
+      "High": 126.050003,
+      "Low": 124.559998,
+      "Close": 125.400002,
+      "Adj Close": 123.713097,
+      "Volume": 4043900
+    },
+    {
+      "Date": "2023-04-25",
+      "Open": 124.900002,
+      "High": 126.190002,
+      "Low": 124.760002,
+      "Close": 125.889999,
+      "Adj Close": 124.196503,
+      "Volume": 4275400
+    },
+    {
+      "Date": "2023-04-26",
+      "Open": 125.809998,
+      "High": 126.550003,
+      "Low": 125.120003,
+      "Close": 125.849998,
+      "Adj Close": 124.157043,
+      "Volume": 4070200
+    },
+    {
+      "Date": "2023-04-27",
+      "Open": 126.370003,
+      "High": 127.019997,
+      "Low": 125.459999,
+      "Close": 126.970001,
+      "Adj Close": 125.261978,
+      "Volume": 3204900
+    },
+    {
+      "Date": "2023-04-28",
+      "Open": 126.580002,
+      "High": 127.25,
+      "Low": 125.639999,
+      "Close": 126.410004,
+      "Adj Close": 124.709511,
+      "Volume": 5060500
+    },
+    {
+      "Date": "2023-05-01",
+      "Open": 126.349998,
+      "High": 126.75,
+      "Low": 126.059998,
+      "Close": 126.089996,
+      "Adj Close": 124.393814,
+      "Volume": 2725000
+    },
+    {
+      "Date": "2023-05-02",
+      "Open": 126.300003,
+      "High": 126.449997,
+      "Low": 123.269997,
+      "Close": 125.160004,
+      "Adj Close": 123.476326,
+      "Volume": 4445300
+    },
+    {
+      "Date": "2023-05-03",
+      "Open": 125.459999,
+      "High": 125.57,
+      "Low": 123.260002,
+      "Close": 123.449997,
+      "Adj Close": 121.78933,
+      "Volume": 4554200
+    },
+    {
+      "Date": "2023-05-04",
+      "Open": 123.029999,
+      "High": 123.519997,
+      "Low": 121.760002,
+      "Close": 122.57,
+      "Adj Close": 120.921165,
+      "Volume": 4468200
+    },
+    {
+      "Date": "2023-05-05",
+      "Open": 123.110001,
+      "High": 124.099998,
+      "Low": 122.809998,
+      "Close": 123.650002,
+      "Adj Close": 121.986641,
+      "Volume": 4970800
+    },
+    {
+      "Date": "2023-05-08",
+      "Open": 123.760002,
+      "High": 123.919998,
+      "Low": 122.550003,
+      "Close": 123.400002,
+      "Adj Close": 121.740005,
+      "Volume": 3663800
+    },
+    {
+      "Date": "2023-05-09",
+      "Open": 121.900002,
+      "High": 121.970001,
+      "Low": 120.660004,
+      "Close": 121.169998,
+      "Adj Close": 121.169998,
+      "Volume": 4540000
+    },
+    {
+      "Date": "2023-05-10",
+      "Open": 121.989998,
+      "High": 122.489998,
+      "Low": 121.099998,
+      "Close": 122.019997,
+      "Adj Close": 122.019997,
+      "Volume": 4189200
+    },
+    {
+      "Date": "2023-05-11",
+      "Open": 122.019997,
+      "High": 122.239998,
+      "Low": 120.550003,
+      "Close": 120.900002,
+      "Adj Close": 120.900002,
+      "Volume": 3446500
+    },
+    {
+      "Date": "2023-05-12",
+      "Open": 121.410004,
+      "High": 122.860001,
+      "Low": 121.110001,
+      "Close": 122.839996,
+      "Adj Close": 122.839996,
+      "Volume": 4560500
+    },
+    {
+      "Date": "2023-05-15",
+      "Open": 123,
+      "High": 123.690002,
+      "Low": 122.339996,
+      "Close": 123.360001,
+      "Adj Close": 123.360001,
+      "Volume": 2915700
+    },
+    {
+      "Date": "2023-05-16",
+      "Open": 123.349998,
+      "High": 123.860001,
+      "Low": 122.449997,
+      "Close": 123.459999,
+      "Adj Close": 123.459999,
+      "Volume": 2748400
+    },
+    {
+      "Date": "2023-05-17",
+      "Open": 123.940002,
+      "High": 125.849998,
+      "Low": 123.470001,
+      "Close": 125.709999,
+      "Adj Close": 125.709999,
+      "Volume": 4515100
+    },
+    {
+      "Date": "2023-05-18",
+      "Open": 125.300003,
+      "High": 126.510002,
+      "Low": 125.190002,
+      "Close": 126.150002,
+      "Adj Close": 126.150002,
+      "Volume": 3797900
+    },
+    {
+      "Date": "2023-05-19",
+      "Open": 126.790001,
+      "High": 128.289993,
+      "Low": 126.550003,
+      "Close": 127.260002,
+      "Adj Close": 127.260002,
+      "Volume": 4305800
+    },
+    {
+      "Date": "2023-05-22",
+      "Open": 127.5,
+      "High": 128.190002,
+      "Low": 127.150002,
+      "Close": 127.5,
+      "Adj Close": 127.5,
+      "Volume": 2806800
+    },
+    {
+      "Date": "2023-05-23",
+      "Open": 127.239998,
+      "High": 129.089996,
+      "Low": 127.129997,
+      "Close": 128.179993,
+      "Adj Close": 128.179993,
+      "Volume": 4592300
+    },
+    {
+      "Date": "2023-05-24",
+      "Open": 127.82,
+      "High": 127.900002,
+      "Low": 125.470001,
+      "Close": 125.68,
+      "Adj Close": 125.68,
+      "Volume": 3915500
+    },
+    {
+      "Date": "2023-05-25",
+      "Open": 125.610001,
+      "High": 127.230003,
+      "Low": 125.010002,
+      "Close": 126.760002,
+      "Adj Close": 126.760002,
+      "Volume": 4102900
+    },
+    {
+      "Date": "2023-05-26",
+      "Open": 127.059998,
+      "High": 129.660004,
+      "Low": 126.809998,
+      "Close": 128.889999,
+      "Adj Close": 128.889999,
+      "Volume": 5612600
+    },
+    {
+      "Date": "2023-05-30",
+      "Open": 129.559998,
+      "High": 130.070007,
+      "Low": 128.259995,
+      "Close": 129.479996,
+      "Adj Close": 129.479996,
+      "Volume": 3741100
+    },
+    {
+      "Date": "2023-05-31",
+      "Open": 128.509995,
+      "High": 129.440002,
+      "Low": 127.459999,
+      "Close": 128.589996,
+      "Adj Close": 128.589996,
+      "Volume": 11590200
+    },
+    {
+      "Date": "2023-06-01",
+      "Open": 128.440002,
+      "High": 130.149994,
+      "Low": 127.779999,
+      "Close": 129.820007,
+      "Adj Close": 129.820007,
+      "Volume": 4136100
+    },
+    {
+      "Date": "2023-06-02",
+      "Open": 130.380005,
+      "High": 133.119995,
+      "Low": 130.149994,
+      "Close": 132.419998,
+      "Adj Close": 132.419998,
+      "Volume": 5373900
+    },
+    {
+      "Date": "2023-06-05",
+      "Open": 133.119995,
+      "High": 133.580002,
+      "Low": 132.270004,
+      "Close": 132.639999,
+      "Adj Close": 132.639999,
+      "Volume": 3993500
+    },
+    {
+      "Date": "2023-06-06",
+      "Open": 132.429993,
+      "High": 132.940002,
+      "Low": 131.880005,
+      "Close": 132.690002,
+      "Adj Close": 132.690002,
+      "Volume": 3298000
+    },
+    {
+      "Date": "2023-06-07",
+      "Open": 132.5,
+      "High": 134.440002,
+      "Low": 132.190002,
+      "Close": 134.380005,
+      "Adj Close": 134.380005,
+      "Volume": 5772000
+    },
+    {
+      "Date": "2023-06-08",
+      "Open": 134.690002,
+      "High": 135.979996,
+      "Low": 134.009995,
+      "Close": 134.410004,
+      "Adj Close": 134.410004,
+      "Volume": 4128500
+    },
+    {
+      "Date": "2023-06-09",
+      "Open": 134.360001,
+      "High": 136.100006,
+      "Low": 134.169998,
+      "Close": 135.300003,
+      "Adj Close": 135.300003,
+      "Volume": 3981500
+    },
+    {
+      "Date": "2023-06-12",
+      "Open": 136,
+      "High": 136.619995,
+      "Low": 135.820007,
+      "Close": 136.419998,
+      "Adj Close": 136.419998,
+      "Volume": 4494500
+    },
+    {
+      "Date": "2023-06-13",
+      "Open": 136.509995,
+      "High": 138.169998,
+      "Low": 136,
+      "Close": 137.600006,
+      "Adj Close": 137.600006,
+      "Volume": 3927300
+    },
+    {
+      "Date": "2023-06-14",
+      "Open": 137.800003,
+      "High": 138.929993,
+      "Low": 136.940002,
+      "Close": 137.199997,
+      "Adj Close": 137.199997,
+      "Volume": 4514900
+    },
+    {
+      "Date": "2023-06-15",
+      "Open": 137.270004,
+      "High": 138.800003,
+      "Low": 137.179993,
+      "Close": 138.399994,
+      "Adj Close": 138.399994,
+      "Volume": 3812600
+    },
+    {
+      "Date": "2023-06-16",
+      "Open": 139.229996,
+      "High": 139.470001,
+      "Low": 137.470001,
+      "Close": 137.479996,
+      "Adj Close": 137.479996,
+      "Volume": 7473100
+    },
+    {
+      "Date": "2023-06-20",
+      "Open": 136.360001,
+      "High": 137.229996,
+      "Low": 135.889999,
+      "Close": 135.960007,
+      "Adj Close": 135.960007,
+      "Volume": 4271300
+    },
+    {
+      "Date": "2023-06-21",
+      "Open": 135.110001,
+      "High": 135.389999,
+      "Low": 133.289993,
+      "Close": 133.690002,
+      "Adj Close": 133.690002,
+      "Volume": 5501300
+    },
+    {
+      "Date": "2023-06-22",
+      "Open": 131.679993,
+      "High": 132.960007,
+      "Low": 130.679993,
+      "Close": 131.169998,
+      "Adj Close": 131.169998,
+      "Volume": 6012800
+    },
+    {
+      "Date": "2023-06-23",
+      "Open": 130.399994,
+      "High": 130.619995,
+      "Low": 129.179993,
+      "Close": 129.429993,
+      "Adj Close": 129.429993,
+      "Volume": 11324300
+    },
+    {
+      "Date": "2023-06-26",
+      "Open": 129.389999,
+      "High": 131.410004,
+      "Low": 129.309998,
+      "Close": 131.339996,
+      "Adj Close": 131.339996,
+      "Volume": 4845600
+    },
+    {
+      "Date": "2023-06-27",
+      "Open": 131.300003,
+      "High": 132.949997,
+      "Low": 130.830002,
+      "Close": 132.339996,
+      "Adj Close": 132.339996,
+      "Volume": 3219900
+    },
+    {
+      "Date": "2023-06-28",
+      "Open": 132.059998,
+      "High": 132.169998,
+      "Low": 130.910004,
+      "Close": 131.759995,
+      "Adj Close": 131.759995,
+      "Volume": 2753800
+    },
+    {
+      "Date": "2023-06-29",
+      "Open": 131.75,
+      "High": 134.350006,
+      "Low": 131.690002,
+      "Close": 134.059998,
+      "Adj Close": 134.059998,
+      "Volume": 3634041
+    }
+   ];
+
+let averageTwenty = [];
+
+/* This for loop cycles through the numbers starting at index of 19 
+(which since the starting index is 0 is the 20th number).
+The nested for loop within tracks backwards to add the previous 19 numbers
+to the 20th number. Then, we get the average by dividing by 20, 
+and push an object containing the date we started with and the average number 
+into a new array.  This repeats for the rest of the numbers. 
+We are working within a 30 day span, with the data set including 
+the previous 20 days. 
+
+In the end we get an array containing the average for each date for
+the 30 day span. */
+
+for (let i = 19; i < data.length; i++) {
+    let sum = data[i].Close;
+    let date = data[i].Date;
+    for (let x = i-1; x > i - 20; x--) {
+        sum = sum + data[x].Close;
+    }
+    averageTwenty.push({date: date, avg: sum/20});
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  let canvas = document.getElementById("twenty-day");
+  let context = canvas.getContext("2d");
+  
+  const canvasHeight = canvas.clientHeight;
+  const canvasWidth = canvas.clientWidth;
+  const chartMargin = 40;
+  const chartWidth = canvasWidth - (chartMargin * 2);
+  const chartHeight = canvasHeight - (chartMargin * 2);
+  const yIncrement = chartHeight / 20;
+  const xIncrement = chartWidth / 30;
+
+  context.strokeStyle = 'black';
+  context.strokeRect(chartMargin, chartMargin, 
+                    chartWidth, chartHeight);
+  
+  //distance from the top of canvas to x axis
+  const yAdjust = canvasHeight - chartMargin;
+  //distance from left of canvas to y axis
+  const xAdjust = chartMargin;
+
+
+  //used closure here to create the numbers on the y-axis
+  const counterY = (() => {
+    count = 120;
+    return (() => {count++; return count;})
+  })();
+
+  //for loop to build ticks along the y axis at set intervals
+  for (let i = chartMargin + yIncrement; i < yAdjust; i += yIncrement) {
+      
+      context.beginPath();
+      context.moveTo(xAdjust, canvasHeight - i);
+      context.lineTo(xAdjust - 10, canvasHeight - i);
+      context.stroke();
+      context.font = "8px Arial";
+      context.fillText(counterY(), xAdjust - 26, canvasHeight + (canvasHeight/240) - i);
+  }
+
+  //for loop to build ticks along the x axis for dates
+  for (let i = chartMargin + xIncrement; i < canvasWidth - xAdjust - 1; i += xIncrement) {
+      context.beginPath();
+      context.moveTo(i, yAdjust);
+      context.lineTo(i, yAdjust + 10);
+      context.stroke();
+  }
+  
+
+  //for loop to fill in dates
+  for (let i = 1; i < averageTwenty.length; i++) {
+      //function to convert dates into more use-able length
+    
+          let split = averageTwenty[i].date.split("-");
+          let month;
+          let day;
+          if (split[1].charAt(0) === "0") {
+              month = split[1].charAt(1);
+          } else {
+              month = split[1];
+          }
+          if (split[2].charAt(0) === "0") {
+              day = split[2].charAt(1);
+          } else {
+              day = split[2];
+          }
+          /* adjusting the position of the dates so they are
+          more uniform, might not work on non static chart */
+          if (month.length === 1 && day.length === 1) {
+            month = " " + month
+          }
+      
+
+      let start = chartMargin - xIncrement/(chartWidth/240);
+      let next = i * xIncrement
+
+      context.font = "8px Arial";
+      context.fillText((month + "/" + day), start + next, yAdjust + 20);
+      
+  }
+
+  // creating grid lines
+
+  //vertical
+  for (let i = chartMargin + xIncrement; i < canvasWidth - xAdjust; i += xIncrement) {
+      context.strokeStyle = 'lightgray';
+      context.moveTo(i, yAdjust);
+      context.lineTo(i, xAdjust);
+      context.stroke();
+  }
+
+  //horizontal
+  for (let i = chartMargin + yIncrement; i < yAdjust; i += yIncrement) {
+    context.beginPath();
+    context.moveTo(xAdjust, canvasHeight - i);
+    context.lineTo(canvasWidth - xAdjust, canvasHeight - i);
+    context.stroke();
+  }
+
+
+  /* creating the line graph */
+  context.strokeStyle = 'blue';
+  context.beginPath();
+  /* since the y scale starts at 120
+  we need to calculate the new position of each point */
+  context.moveTo(xAdjust, yAdjust - ((averageTwenty[0].avg - 120) * yIncrement));
+  for (let i = 1; i < averageTwenty.length; i++) {
+      const start = xAdjust;
+      let next = xIncrement * i;
+      context.lineTo (start + next, yAdjust - ((averageTwenty[i].avg - 120) * yIncrement));
+      context.stroke()
+  }
+
+  // creating dots on the graph //
+  for (let i = 1; i < averageTwenty.length; i++) {
+    context.beginPath()
+    const start = xAdjust;
+    let next = xIncrement * i;
+    context.arc(start + next, yAdjust - ((averageTwenty[i].avg - 120) * yIncrement), 2, 0, 2* Math.PI);
+    context.fill()
+    context.stroke()
+}
+  // creating hover elements with targets //
+  for (let i = 1; i < averageTwenty.length; i++) {
+    let popup = document.createElement('div');
+    let target = document.createElement('div')
+    let content = document.createTextNode(averageTwenty[i].avg.toString().slice(0,10));
+    let main = document.getElementById('canvas-container');
+    popup.setAttribute('class', 'popup');
+    target.setAttribute('class', 'target')
+    popup.setAttribute("id", "popup" + i)
+    target.setAttribute('id', 'target' + i)
+    popup.appendChild(content);
+    main.appendChild(target);
+    main.appendChild(popup);
+  }
+
+  //adjusting hover elements
+  const hovers = document.querySelectorAll('.popup');
+  hovers.forEach((hover, index)=>{
+    const start = xAdjust;
+    let next = xIncrement * index;
+      hover.style.fontSize = "12px";
+      hover.style.transform = `translate(${start + next}px, ${yAdjust - ((averageTwenty[index+1].avg - 120) * yIncrement)}px)`
+  })
+  // adjusting target elements
+  const targets = document.querySelectorAll('.target');
+  targets.forEach((target, index)=>{
+    const start = xAdjust;
+    let next = xIncrement * index;
+      target.style.transform = `translate(${start + next}px, ${yAdjust - ((averageTwenty[index+1].avg - 120) * yIncrement)}px)`
+  })
+
+  // adding rules for hover
+  const style = document.createElement('style')
+
+  for (let i = 1; i < averageTwenty.length; i++) {
+    let css = `#target${i}:hover + #popup${i} { opacity: 100; }`
+    
+    style.appendChild(document.createTextNode(css))
+    
+    document.getElementsByTagName('head')[0].appendChild(style);
+  }
+
+  
+
+}) 
